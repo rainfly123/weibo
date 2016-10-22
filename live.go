ecd386f1f0342212270f4174c1ec77460c73a4df
rainfly <xiechc@gmail.com>
2016-10-22 13:37:55 +0800

    16:9

--- live.go
+++ live.go
@@ -37,7 +37,7 @@ func Checkvideo(redis_key string, origin string) {
 	index := strings.LastIndex(origin, ".")
 	if index > 0 {
 		dest := origin[0:index+1] + "jpg"
-		var args = []string{"-i", origin, "-vframes", "1", "-vf", "crop=iw:iw/2", "-f", "image2", "-y", dest}
+		var args = []string{"-i", origin, "-vframes", "1", "-vf", "crop=iw:iw*9/16", "-f", "image2", "-y", dest}
 		cmd := exec.Command("ffmpeg", args[0:]...)
 		//cmd.Stdout = os.Stdout
 		//cmd.Stderr = os.Stderr
